6bf471af7087daabf5c0ea7a1f3e0863715fc36b
mehmetmalgir <mmalgr@gmail.com>
2023-04-18 19:03:54 +0300

    All Layers Created. Core Layer, Entities and IGenericRepository Created.

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+	public interface IGenericRepository<T> where T : class
+	{
+		//IQueryable'da veritabanına sprgu atmaz ne zamanki ToList(); deriz o zaman db'ye sorguyu atar. Dolayısıyla listeleyip vs sonra veritabanına atabiliriz.Daha hızlı olur.
+
+		IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+		Task<T> GetByIdAsync(int id);
+		IQueryable<T> Where(Expression<Func<T, bool>> expression);
+		Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+		Task AddAsync(T entity);
+		Task AddRangeAsync(IEnumerable<T> entities);
+		void Upgrade(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entities);
+
+	}
+}
